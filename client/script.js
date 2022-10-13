9386f318c8a5fa11017df1cc20f91cab85090a33
George Ogola <gogola89@gmail.com>
2022-10-13 16:01:51 +0300

    refactor js file

--- client/script.js
+++ client/script.js
@@ -1,68 +1,80 @@
 'use strict';
 
-let score = 20;
-let cumScore = 0;
-let highScore = 0;
+let points = 20;
+let cumPoints = 0;
+let highestPoints = 0;
 let secretNumber = Math.floor(Math.random() * 20) + 1;
 
-document.querySelector('.again').disabled = true;
+
+const head = document.querySelector('.head');
+const number = document.querySelector('.number');
+const highscore = document.querySelector('.highscore');
+const cumscore = document.querySelector('.cumscore');
+const body = document.querySelector('body');
+const again = document.querySelector('.again');
+const check = document.querySelector('.check');
+const guess = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+
+again.disabled = true;
 
 const displayMessage = message => {
-  document.querySelector('.message').textContent = message;
+  message.textContent = message;
 };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+check.addEventListener('click', function () {
+  const guessNumber = Number(guess.value);
 
   // When the guess is below 1 or no input
-  if (guess < 1) {
+  if (guessNumber < 1) {
     displayMessage('Enter number greater than 0!');
   }
   // When the guess exceeds 20
-  else if (guess > 20) {
+  else if (guessNumber > 20) {
     displayMessage('Number should not exceed 20!');
   }
 
   // When the guess is right
-  else if (guess === secretNumber) {
-    cumScore += score;
-    if (score > highScore) highScore = score;
+  else if (guessNumber === secretNumber) {
+    cumPoints += points;
+    if (points > highestPoints) highestPoints = points;
     displayMessage('💥 Correct Number!');
-    document.querySelector('.head').textContent = 'You Won!';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.highscore').textContent = highScore;
-    document.querySelector('.cumscore').textContent = cumScore;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.again').disabled = false;
-    document.querySelector('.check').disabled = true;
-    document.querySelector('.guess').disabled = true;
+    head.textContent = 'You Won!';
+    number.textContent = secretNumber;
+    highscore.textContent = highestPoints;
+    cumscore.textContent = cumPoints;
+    body.style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+    again.disabled = false;
+    check.disabled = true;
+    guess.disabled = true;
   }
   // When the guess is wrong
-  else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess < secretNumber ? '↙️ Too low!' : '↗️ Too high!');
-      score--;
-      document.querySelector('.score').textContent = score;
+  else if (guessNumber !== secretNumber) {
+    if (points > 1) {
+      displayMessage(guessNumber < secretNumber ? '↙️ Too low!' : '↗️ Too high!');
+      points--;
+      score.textContent = points;
     } else {
       displayMessage('⚠️ You lost!');
-      document.querySelector('.score').textContent = 0;
+      score.textContent = 0;
     }
   }
 });
 // When you restart the game
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
+  points = 20;
   secretNumber = Math.floor(Math.random() * 20) + 1;
-  document.querySelector('.again').disabled = true;
-  document.querySelector('.check').disabled = false;
-  document.querySelector('.guess').disabled = false;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.head').textContent = 'Guess My Number!';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+  again.disabled = true;
+  check.disabled = false;
+  guess.disabled = false;
+  body.style.backgroundColor = '#222';
+  head.textContent = 'Guess My Number!';
+  number.textContent = '?';
+  guess.value = '';
   displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.highscore').textContent = highestScore;
-  document.querySelector('.number').style.width = '15rem';
+  score.textContent = points;
+  highscore.textContent = highestPoints;
+  number.style.width = '15rem';
 });
